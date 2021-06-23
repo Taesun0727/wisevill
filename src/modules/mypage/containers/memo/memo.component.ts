010e634b13ec01cc619657dca2cd6e13e46e4726
parkinhee <48777463+parkinhee@users.noreply.github.com>
2021-06-23 17:14:14 +0900

    memo

--- src/modules/mypage/containers/memo/memo.component.ts
+++ src/modules/mypage/containers/memo/memo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'sb-memo',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memo.component.scss']
 })
 export class MemoComponent implements OnInit {
+  closeResult = '';
+  constructor(private modalService: NgbModal) { }
 
-  constructor() { }
+ 
+  
 
   ngOnInit(): void {
+  }
+  open(content : any) {
+    console.log(content)
+    this.modalService.open(content, {ariaDescribedBy: 'modal-basic-title', centered: true,  size: 'lg' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    })
+  }
+
+  private getDismissReason(reason : any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
