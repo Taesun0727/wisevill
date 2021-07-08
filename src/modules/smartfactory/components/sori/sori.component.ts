6f435c62b8178d7c48e716acb30455f0dcb3ce19
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-08 17:45:24 +0900

    (폼변경)모니터링 차트

---
+++ src/modules/smartfactory/components/sori/sori.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sb-sori',
+  templateUrl: './sori.component.html',
+  styleUrls: ['./sori.component.scss']
+})
+export class SoriComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
