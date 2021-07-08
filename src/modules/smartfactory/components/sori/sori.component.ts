5d8b7e1f3a8a621d59513abf994f538d963223ec
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-08 17:47:23 +0900

    (폼변경)모니터링 차트

--- src/modules/smartfactory/components/sori/sori.component.ts
+++ src/modules/smartfactory/components/sori/sori.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  
+  ElementRef,
+
+  ViewChild,
+} from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'sb-sori',
   templateUrl: './sori.component.html',
   styleUrls: ['./sori.component.scss']
 })
-export class SoriComponent implements OnInit {
+export class SoriComponent implements OnInit, AfterViewInit {
+  @ViewChild('myPieChart') myPieChart!: ElementRef<HTMLCanvasElement>;
+  chart!: Chart;
 
-  constructor() { }
+  constructor() {}
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+      this.chart = new Chart(this.myPieChart.nativeElement, {
+          type: 'pie',
+          data: {
+              labels: ['Blue', 'Red', 'Yellow', 'Green'],
+              datasets: [
+                  {
+                      data: [12.21, 15.58, 11.25, 8.32],
+                      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+                  },
+              ],
+          },
+      });
   }
 
 }
