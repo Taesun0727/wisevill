4b3a8ce2e73694752b9b7f82c7cc2cfe3c8ba126
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-08 16:27:36 +0900

    (폼변경)모니터링

--- src/modules/smartfactory/containers/monitoring/monitoring.component.ts
+++ src/modules/smartfactory/containers/monitoring/monitoring.component.ts
@@ -1,22 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, NgModule, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+ 
+  ElementRef,
+ 
+  ViewChild,
+} from '@angular/core';
+import { Chart } from 'chart.js';
+
+
 
 @Component({
   selector: 'sb-monitoring',
   templateUrl: './monitoring.component.html',
   styleUrls: ['./monitoring.component.scss']
 })
-export class MonitoringComponent implements OnInit {
+export class MonitoringComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
-  company: any
 
-  ngOnInit(): void {
-    this.company = history.state.company;
-    this.searchmonitor();
-  }
+  @ViewChild('myAreaChart') myAreaChart!: ElementRef<HTMLCanvasElement>;
+  chart!: Chart;
 
-  searchmonitor() {
-    console.log(this.company);
+  constructor() {}
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+      this.chart = new Chart(this.myAreaChart.nativeElement, {
+          type: 'line',
+          data: {
+              labels: [
+                  'Mar 1',
+                  'Mar 2',
+                  'Mar 3',
+                  'Mar 4',
+                  'Mar 5',
+                  'Mar 6',
+                  'Mar 7',
+                  'Mar 8',
+                  'Mar 9',
+                  'Mar 10',
+                  'Mar 11',
+                  'Mar 12',
+                  'Mar 13',
+              ],
+              datasets: [
+                  {
+                      label: 'Sessions',
+                      lineTension: 0.3,
+                      backgroundColor: 'rgba(2,117,216,0.2)',
+                      borderColor: 'rgba(2,117,216,1)',
+                      pointRadius: 5,
+                      pointBackgroundColor: 'rgba(2,117,216,1)',
+                      pointBorderColor: 'rgba(255,255,255,0.8)',
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: 'rgba(2,117,216,1)',
+                      pointHitRadius: 50,
+                      pointBorderWidth: 2,
+                      data: [
+                          10000,
+                          30162,
+                          26263,
+                          18394,
+                          18287,
+                          28682,
+                          31274,
+                          33259,
+                          25849,
+                          24159,
+                          32651,
+                          31984,
+                          38451,
+                      ],
+                  },
+              ],
+          },
+          options: {
+              scales: {
+                  xAxes: [
+                      {
+                          time: {
+                              unit: 'day',
+                          },
+                          gridLines: {
+                              display: false,
+                          },
+                          ticks: {
+                              maxTicksLimit: 7,
+                          },
+                      },
+                  ],
+                  yAxes: [
+                      {
+                          ticks: {
+                              min: 0,
+                              max: 40000,
+                              maxTicksLimit: 5,
+                          },
+                          gridLines: {
+                              color: 'rgba(0, 0, 0, .125)',
+                          },
+                      },
+                  ],
+              },
+              legend: {
+                  display: false,
+              },
+          },
+      });
   }
 
 }
