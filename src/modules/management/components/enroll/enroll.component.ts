15dfd2e1d8fd4dcc7a933c5f7938846f37611134
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-14 17:53:11 +0900

    (수정)사원등록 수정

--- src/modules/management/components/enroll/enroll.component.ts
+++ src/modules/management/components/enroll/enroll.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Member } from 'modules/management/models/member'
 @Component({
   selector: 'sb-enroll',
   templateUrl: './enroll.component.html',
@@ -11,5 +11,16 @@ export class EnrollComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  Checks: Array<any> = [
+		{ name: '와이즈빌1', details: [  '개발팀','영업팀', ] },
+		{ name: '와이즈빌2', details: [  '홍보팀','행정팀',  ] }
+	];
+    
+	details: Array<any> | undefined; 
+
+	changeCheck(check: any) { 
+		
+		this.details = this.Checks.find((chk: any) => chk.name == check).details; //Angular 11
+	}
 
 }
