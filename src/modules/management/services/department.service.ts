09db373ad31cc91ba910b3c3911e0b5bfcd03c4a
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-16 13:51:33 +0900

    (기능추가) 파이어베이스 추가

---
+++ src/modules/management/services/department.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+  dbPath = '/department';
+
+  constructor() { }
+}
