f8971ccf9cdf031e76463547ec4ed2af2e40ddb1
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-20 17:02:05 +0900

    (추가)기능추가

--- src/modules/smartfactory/services/company.service.ts
+++ src/modules/smartfactory/services/company.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireObject } from '@angular/fire/database';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Company } from '@modules/management/models/company';
+import { Company } from '@modules/smartfactory/models/company';
 
 @Injectable({
   providedIn: 'root'
