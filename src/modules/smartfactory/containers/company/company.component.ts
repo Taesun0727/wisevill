2361a474a1d2558b1983a50c4fe8a60e32ef173c
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-21 17:46:46 +0900

    (수정) 삭제기능 추가

--- src/modules/smartfactory/containers/company/company.component.ts
+++ src/modules/smartfactory/containers/company/company.component.ts
@@ -33,6 +33,13 @@ export class CompanyComponent implements OnInit {
   isAllCheckBoxChecked() {
     return this.companies.every(p => p.checked);
   }
+  listDelSubmit(x: any){
+    var delBtn = confirm(" Do you want to delete ?");
+    if ( delBtn == true ) {
+      this.companies.splice(x, 1 );
+    }   
+  }
+  
 
   refreshCountries() {
     this.companies = COMPANIES
