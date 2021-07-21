565a5e2c7c82f1150d145f050497703cf73b52f0
parkinhee <dlsgmlcjstk925@hanmail.net>
2021-07-21 13:17:03 +0900

    (기능추가)DB추가

--- src/modules/smartfactory/components/createiot/createiot.component.ts
+++ src/modules/smartfactory/components/createiot/createiot.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { IotService } from '@modules/smartfactory/services/iot.service';
 
 @Component({
   selector: 'sb-createiot',
@@ -8,16 +10,55 @@ import { Router } from '@angular/router';
 })
 export class CreateiotComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  public iotsForm: FormGroup|any;
+
+  constructor(public router: Router,public iotcrud: IotService, public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.iotForm();
+    console.log(this.iotcrud.getAll())
   }
+ 
+  iotForm() {
+    this.iotsForm = this.fb.group({
+      cname:[''],
+      name:[''],
+      type:[''],
+      content:[''],
+      rssi:[''],
+      status:[''],
+      checked: [''],
+    })
+  }
+ 
 
+  get cname() {
+    return this.iotsForm.get('company')
+  }
+  get name() {
+    return this.iotsForm.get('type')
+  }
+  get type() {
+    return this.iotsForm.get('cnserver')
+  }
+  get rssi() {
+    return this.iotsForm.get('name')
+  }
+  get status() {
+    return this.iotsForm.get('host')
+  }
+  
+  ResetForm() {
+    this.iotsForm.reset();
+  }
+ 
   golist() {
     this.router.navigateByUrl('/smartfactory/iot')
   }
-  
+
   save() {
+    this.iotcrud.AddIot(this.iotsForm.value);
     this.router.navigateByUrl('/smartfactory/iot')
   }
+
 }
